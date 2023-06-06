e3b41efb270b2d776de48207b51b0bc8e7e7ddf8
my-amtech <ammuftau74@gmail.com>
2023-06-06 10:20:53 +0100

    remove password validator

--- utils/transporter.js
+++ utils/transporter.js
@@ -1,11 +1,11 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
-nodemailer.createTransport({
-    service:"gmail",
-    auth:{
-        user:process.env.EMAIL,
-        pass:process.env.PASSWORD
-    }
-}) 
+let transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: process.env.EMAIL,
+		pass: process.env.PASSWORD,
+	},
+});
 
-module.exports = transporter
+module.exports = transporter;
