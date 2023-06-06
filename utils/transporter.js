20b5fae7f6133acca87b2f9a4fe406cd5c5f464b
my-amtech <ammuftau74@gmail.com>
2023-06-06 10:39:09 +0100

    transporter password validator

--- utils/transporter.js
+++ utils/transporter.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 
-let transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
 	service: 'gmail',
 	auth: {
 		user: process.env.EMAIL,
